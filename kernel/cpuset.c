07699319f1240273ada5075cb4478a74c1760097
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-02-28 10:40:39 +0530

    cpuset: Restore tasks affinity while moving across cpusets
    When tasks move across cpusets, the current affinity settings
    are lost. Cache the task affinity and restore it during cpuset
    migration. The restoring happens only when the cached affinity
    is subset of the current cpuset settings.
    
    Change-Id: I6c2ec1d5e3d994e176926d94b9e0cc92418020cc
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: nysascape <jago@nysascape.digital>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -856,6 +856,20 @@ void rebuild_sched_domains(void)
 	put_online_cpus();
 }
 
+static int update_cpus_allowed(struct cpuset *cs, struct task_struct *p,
+			       const struct cpumask *new_mask)
+{
+	int ret;
+
+	if (cpumask_subset(&p->cpus_requested, cs->cpus_requested)) {
+		ret = set_cpus_allowed_ptr(p, &p->cpus_requested);
+		if (!ret)
+			return ret;
+	}
+
+	return set_cpus_allowed_ptr(p, new_mask);
+}
+
 /**
  * update_tasks_cpumask - Update the cpumasks of tasks in the cpuset.
  * @cs: the cpuset in which each task's cpus_allowed mask needs to be changed
@@ -871,7 +885,7 @@ static void update_tasks_cpumask(struct cpuset *cs)
 
 	css_task_iter_start(&cs->css, &it);
 	while ((task = css_task_iter_next(&it)))
-		set_cpus_allowed_ptr(task, cs->effective_cpus);
+		update_cpus_allowed(cs, task, cs->effective_cpus);
 	css_task_iter_end(&it);
 }
 
@@ -1554,7 +1568,7 @@ static void cpuset_attach(struct cgroup_taskset *tset)
 		 * can_attach beforehand should guarantee that this doesn't
 		 * fail.  TODO: have a better way to handle failure here
 		 */
-		WARN_ON_ONCE(set_cpus_allowed_ptr(task, cpus_attach));
+		WARN_ON_ONCE(update_cpus_allowed(cs, task, cpus_attach));
 
 		if (!strncmp(name_buf, "background", 10))
 			set_user_nice(task, MAX_NICE);
