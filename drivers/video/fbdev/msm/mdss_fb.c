c869dcf623a47d31f06dd3ee32d87e8cfc7375d7
LibXZR <xzr467706992@163.com>
2019-11-16 17:48:49 +0800

    mdss_fb: Implement boost_control

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -47,6 +47,8 @@
 #include <linux/kthread.h>
 #include <linux/dma-buf.h>
 #include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
+#include <linux/boost_control.h>
 #include <linux/interrupt.h>
 #include <linux/wakelock.h>
 #include <linux/mdss_io_util.h>
@@ -5290,8 +5292,14 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
-		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+		#if CONFIG_BOOST_CONTROL
+		if(enable_fb_boost){
+		#endif
 		cpu_input_boost_kick();
+		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+		#if CONFIG_BOOST_CONTROL
+		}
+		#endif
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
