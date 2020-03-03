82cb1b5bcc49feea73f0da1d42e717c3737ad540
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-03-03 23:56:45 +0530

    cpu_input_boost: Ensure that we are using EAS while screen off
    Additionally, add a comment supporting enabling of EAS.
    
    Reported-by: PrimoDev23 <lexx.ps2711@gmail.com>
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -225,6 +225,7 @@ static int cpu_notifier_cb(struct notifier_block *nb, unsigned long action,
 	/* Unboost when the screen is off */
 	if (test_bit(SCREEN_OFF, &b->state)) {
 		policy->min = policy->cpuinfo.min_freq;
+		sysctl_sched_energy_aware = 1;
 		return NOTIFY_OK;
 	}
 
@@ -244,6 +245,7 @@ static int cpu_notifier_cb(struct notifier_block *nb, unsigned long action,
 	else
 		policy->min = policy->cpuinfo.min_freq;
 
+	/* If we are not boosting max for app launch/device wake, enable EAS */
 	sysctl_sched_energy_aware = 1;
 
 	return NOTIFY_OK;
